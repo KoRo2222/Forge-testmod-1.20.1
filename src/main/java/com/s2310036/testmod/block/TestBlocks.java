33f2e3c353a39fff97e509d0cf1a1d1e21c50175
KoRo2 <koro2sub@gmail.com>
2025-05-04 21:46:35 +0900

    基本ブロックのblockitem(giveで呼び出せるように)を追加

--- src/main/java/com/s2310036/testmod/block/TestBlocks.java
+++ src/main/java/com/s2310036/testmod/block/TestBlocks.java
@@ -1,6 +1,9 @@
 package com.s2310036.testmod.block;
 
 import com.s2310036.testmod.TestMod;
+import com.s2310036.testmod.item.TestItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
@@ -10,12 +13,22 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class TestBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, TestMod.MOD_ID);
 
-    public static final RegistryObject<Block> GUARDMARINE = BLOCKS.register("guardmarine",
+    public static final RegistryObject<Block> GUARDMARINE = registerBlockItem("guardmarine",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK).sound(SoundType.AMETHYST)));
+
+    private static <T extends Block> RegistryObject<T> registerBlockItem(String name,
+                                                                          Supplier<T> supplier) {
+        RegistryObject<T> block = BLOCKS.register(name, supplier);
+        TestItems.ITEMS.register(name,
+                () -> new BlockItem(block.get(), new Item.Properties()));
+        return block;
+    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
